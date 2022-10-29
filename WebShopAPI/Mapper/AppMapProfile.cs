af2a3d63d890c34a3784db83ffa524eb22fe5e9a
Ig2525 <ig25@ukr.net>
2022-10-29 13:36:48 +0300

    Настроїли Сервер. Створюються запити json.

---
+++ WebShopAPI/Mapper/AppMapProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using LibData.Entities;
+using WebShopAPI.Models;
+
+namespace WebShopAPI.Mapper
+{
+    public class AppMapProfile: Profile
+    {
+        public AppMapProfile()
+        {
+            CreateMap<CategoryEntity, CategoryItemViewModel>()
+                .ForMember(x => x.Image, opt => opt.MapFrom(cat => $"/images/{cat.Image}"));
+        }
+    }
+}
