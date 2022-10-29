b009dc09494bcdec1ccf3f6ffdd87a58ad40ece4
Ig2525 <ig25@ukr.net>
2022-10-29 12:42:58 +0300

    Добавьте файлы проекта.

---
+++ WebShopAPI/Services/SeederDB.cs
@@ -0,0 +1,38 @@
+﻿using LibData.Entities;
+using LibData;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebShopAPI.Services
+{
+    public static class SeederDB
+    {
+        public static void SeedData(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<AppEFContext>();
+                context.Database.Migrate();
+
+                if (!context.Categories.Any())
+                {
+                    Dictionary<string, string> categories = new Dictionary<string, string> {
+                        { "Ноутбуки", "laptop.jpg" },
+                        { "Монітори", "monotor.jpg"},
+                        { "Одяг", "clothing.jpg" }
+                    };
+                    foreach (var item in categories)
+                    {
+                        CategoryEntity cat = new CategoryEntity
+                        {
+                            Name = item.Key,
+                            Image = item.Value,
+                            DateCreated = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc)
+                        };
+                        context.Categories.Add(cat);
+                        context.SaveChanges();
+                    }
+                }
+            }
+        }
+    }
+}
